df53580fe557da5ab1e877fa5fff87322ef6ee5b
Jay Bell <jordanbell321@gmail.com>
2026-01-28 21:38:43 +0000

    Add Podfile.properties.json to configure Expo with Hermes engine and enable network inspector

--- app.config.js
+++ app.config.js
@@ -1,60 +1,67 @@
+import "dotenv/config";
+
 export default {
-    expo: {
-        name: "Echo Map",
-        slug: "echo-map",
-        version: "1.0.0",
-        orientation: "portrait",
-        icon: "./assets/icon.png",
-        userInterfaceStyle: "light",
-        splash: {
-            image: "./assets/splash-icon.png",
-            resizeMode: "contain",
-            backgroundColor: "#ffffff",
-        },
-        ios: {
-            supportsTablet: true,
-            infoPlist: {
-                NSLocationWhenInUseUsageDescription:
-                    "EchoMap needs your location to guide you to audio nodes and show your position on the map.",
-                NSLocationAlwaysAndWhenInUseUsageDescription:
-                    "EchoMap needs your location to guide you to audio nodes even when the app is in the background.",
-            },
-            config: {
-                googleMapsApiKey:
-                    process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY ||
-                    "YOUR_GOOGLE_MAPS_API_KEY_HERE",
-            },
-        },
-        android: {
-            adaptiveIcon: {
-                foregroundImage: "./assets/adaptive-icon.png",
-                backgroundColor: "#ffffff",
-            },
-            edgeToEdgeEnabled: true,
-            permissions: [
-                "ACCESS_FINE_LOCATION",
-                "ACCESS_COARSE_LOCATION",
-                "VIBRATE",
-            ],
-            config: {
-                googleMaps: {
-                    apiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
+        expo: {
+                name: "Echo Map",
+                slug: "echo-map",
+                version: "0.1.0",
+                orientation: "portrait",
+                icon: "./src/assets/icon.png",
+                userInterfaceStyle: "light",
+                splash: {
+                        image: "./src/assets/splash-icon.png",
+                        resizeMode: "contain",
+                        backgroundColor: "#ffffff",
                 },
-            },
-        },
-        web: {
-            favicon: "./assets/favicon.png",
-        },
-        plugins: [
-            [
-                "expo-location",
-                {
-                    locationAlwaysAndWhenInUsePermission:
-                        "Allow EchoMap to use your location to guide you to audio nodes.",
+                ios: {
+                        supportsTablet: true,
+                        infoPlist: {
+                                NSLocationWhenInUseUsageDescription:
+                                        "EchoMap needs your location to guide you to audio nodes and show your position on the map.",
+                                NSLocationAlwaysAndWhenInUseUsageDescription:
+                                        "EchoMap needs your location to guide you to audio nodes even when the app is in the background.",
+                        },
+                        config: {
+                                googleMapsApiKey:
+                                        process.env
+                                                .EXPO_PUBLIC_GOOGLE_MAPS_API_KEY ||
+                                        "YOUR_GOOGLE_MAPS_API_KEY_HERE",
+                        },
+                        bundleIdentifier: "com.jaynightmare.echomap",
                 },
-            ],
-            "expo-audio",
-            "expo-video",
-        ],
-    },
+                android: {
+                        adaptiveIcon: {
+                                foregroundImage:
+                                        "./src/assets/adaptive-icon.png",
+                                backgroundColor: "#ffffff",
+                        },
+                        edgeToEdgeEnabled: true,
+                        permissions: [
+                                "ACCESS_FINE_LOCATION",
+                                "ACCESS_COARSE_LOCATION",
+                                "VIBRATE",
+                        ],
+                        config: {
+                                googleMaps: {
+                                        apiKey: process.env
+                                                .EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
+                                },
+                        },
+                        package: "com.jaynightmare.echomap",
+                },
+                web: {
+                        favicon: "./src/assets/favicon.png",
+                },
+                plugins: [
+                        [
+                                "expo-location",
+                                {
+                                        locationAlwaysAndWhenInUsePermission:
+                                                "Allow EchoMap to use your location to guide you to audio nodes.",
+                                },
+                        ],
+                        "expo-audio",
+                        "expo-video",
+                ],
+        },
 };
